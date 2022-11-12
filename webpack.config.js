5ad8e0f52bbb07c445876ca55939f6b23bb79799
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-11-12 07:12:31 +0100

    Update project file structure

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    assetModuleFilename: 'assets/[name].[ext]'
+    assetModuleFilename: 'assets/[name][ext]'
   },
   optimization: {
     runtimeChunk: 'single',
