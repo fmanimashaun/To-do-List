564fd28f9a58c3e5801c48231fcaae492a005446
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-11-12 16:02:11 +0100

    Update project function

--- src/js/modules/displayTaskList.js
+++ src/js/modules/displayTaskList.js
@@ -50,43 +50,6 @@ export const displayTodo = (list) => {
     dragBtn.alt = 'delete icon';
     dragBtn.className = 'todo__btn-drag';
 
-    // /* add event listener to the textarea for focus */
-    // todo.addEventListener('focus', (event) => {
-    //   event.target.parentElement.classList.add('focus');
-    //   event.target.nextElementSibling.classList.add('show');
-    // });
-
-    // /* add event listener to the textarea when not focus */
-    // todo.addEventListener('blur', (event) => {
-    //   event.target.parentElement.classList.remove('focus');
-    //   event.target.nextElementSibling.classList.remove('show');
-    // });
-
-    // /* add event listener to the check button */
-    // taskCheckBtn.addEventListener('click', () => {
-    //   taskCheckBtn.classList.toggle('checked');
-    //   taskCheckBtn.nextElementSibling.classList.toggle('completed');
-    //   task.completed = !(task.completed);
-    // });
-
-    // /* adding event listener to textarea when focus */
-    // todo.addEventListener('focus', (event) => {
-    //   event.target.parentElement.classList.add('focus');
-    //   const dragBtnEl = event.target.nextElementSibling;
-    //   const deleteBtnEl = dragBtnEl.nextElementSibling;
-    //   dragBtnEl.classList.add('hide');
-    //   deleteBtnEl.classList.add('show');
-    // });
-
-    // /* adding event listener to textarea when not focus */
-    // todo.addEventListener('blur', (event) => {
-    //   event.target.parentElement.classList.remove('focus');
-    //   const dragBtnEl = event.target.nextElementSibling;
-    //   const deleteBtnEl = dragBtnEl.nextElementSibling;
-    //   dragBtnEl.classList.remove('hide');
-    //   deleteBtnEl.classList.remove('show');
-    // });
-
     /* Appending elements to parents */
     taskCheckBtn.appendChild(taskCheckBtnImg);
     taskDiv.appendChild(taskCheckBtn);
