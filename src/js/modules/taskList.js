1900008b95ef26360448ecaeab996f5ff99a3d84
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-11-17 11:10:59 +0100

    Add comments to code

--- src/js/modules/taskList.js
+++ src/js/modules/taskList.js
@@ -28,27 +28,52 @@ export default class TaskList {
     /* get the nodeList of task been displayed */
     const todoTastList = document.querySelector('.todo__list');
 
+    /* get the current task list */
     const currentTasks = this.getTasks();
 
+    /* add event listener to the task list container */
     todoTastList.addEventListener('click', (event) => {
+      /* check if the event target is the textarea */
       if (event.target.matches('textarea')) {
+        /* get the input label element */
         const todoLabel = event.target.parentElement;
+
+        /* get the label parent div */
         const todoDiv = todoLabel.parentElement;
+
+        /* add a focus class to the label parent div to display the delete btn */
         todoDiv.classList.add('focus');
       } else if (event.target.classList.contains('todo__btn-delete')) {
+        /* check if number of task is one when delete btn clicked */
         if (currentTasks.length === 1) {
+          /* clear the page */
           todoTastList.innerHTML = '';
+
+          /* remove the last task from the task list and update localstorage */
           currentTasks.splice(0, 1);
           localStorage.setItem('taskList', JSON.stringify(currentTasks));
+
+          /* display empty message */
           this.display();
         } else {
+          /* get the input label element */
           const todoLabel = event.target.parentElement;
+
+          /* get the label for attribute value */
           const todoLabelAtr = todoLabel.getAttribute('for');
+
+          /* get the index of the task to be deleted */
           const todoIndex = todoLabelAtr.split('-')[1] - 1;
+
+          /* get the label parent div */
           const todoDiv = todoLabel.parentElement;
+
+          /* remove the task from the task list and update localstorage */
           todoDiv.parentElement.removeChild(todoDiv);
           currentTasks.splice(todoIndex, 1);
           localStorage.setItem('taskList', JSON.stringify(currentTasks));
+
+          /* update the index of the task in the task list */
           this.display();
         }
       }
