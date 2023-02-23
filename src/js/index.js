dbccfc3f1f56a7614abe13854950f6082505b566
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-02-23 22:20:28 +0100

    Reset code

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,3 @@
 import '../css/reset.css';
 import '../scss/styles.scss';
 import displayTodos from './modules/displayTaskList.js';
-
-// create an araa of objects to be used as the task list
-const taskList = [
-  {
-    description: 'Task 1',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Task 2',
-    completed: true,
-    index: 2,
-  },
-  {
-    description: 'Task 3',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'Task 4',
-    completed: true,
-    index: 4,
-  },
-];
-
-// display the task list on page load
-document.addEventListener('DOMContentLoaded', () => {
-  displayTodos(taskList);
-});
