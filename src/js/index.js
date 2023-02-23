a292882ceea84b69b44d3dbca2d373da1a3cb94e
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-02-23 22:21:59 +0100

    Create an array of objects (tasks)

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,27 @@
 import '../css/reset.css';
 import '../scss/styles.scss';
 import displayTodos from './modules/displayTaskList.js';
+
+// create an araa of objects to be used as the task list
+const taskList = [
+  {
+    description: 'Task 1',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Task 2',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'Task 3',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Task 4',
+    completed: true,
+    index: 4,
+  },
+];
