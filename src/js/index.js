ce351342a5807a8e4f1f0ce13fe5031fa1bf7db2
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-02-24 22:05:14 +0100

    remove all hard coded texts

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,4 @@
 import '../scss/styles.scss';
 import displayTodos from './modules/displayTodos.js';
 
-// create an araa of objects to be used as the task list
-const taskList = [
-  {
-    description: 'Task 1',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Task 2',
-    completed: true,
-    index: 2,
-  },
-  {
-    description: 'Task 3',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'Task 4',
-    completed: true,
-    index: 4,
-  },
-];
-
-// call the displayTodos function to display the task list when the page loads
-document.addEventListener('DOMContentLoaded', () => {
-  displayTodos(taskList);
-});
+const todoForm = document.querySelector('.todos__form');
