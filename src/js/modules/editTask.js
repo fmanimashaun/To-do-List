1ecd6ed537fbbf91ab9211c0bae0373f5d0f9413
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-02-25 13:25:08 +0100

    Update editTask function

--- src/js/modules/editTask.js
+++ src/js/modules/editTask.js
@@ -4,11 +4,11 @@ const editTask = (id, str) => {
   // get list of tasks from local storage
   const tasks = JSON.parse(localStorage.getItem('tasks'));
 
-  // find the task to edit
-  const taskIndex = tasks.findIndex((task) => task.id === id);
+  // get the task to edit
+  const task = tasks.find((task) => task.id === id);
 
   // update the task description
-  tasks[taskIndex].description = str;
+  task.description = str;
 
   // save the updated task list to local storage
   localStorage.setItem('tasks', JSON.stringify(tasks));
