a6b5cabd58f4596f36eadda829ed03dd38eee4ee
Jordan <camiloarmijosantifonamusic@gmail.com>
2023-03-03 12:28:32 -0500

    Create test for "Clear all completed" function

--- __tests__/add-remove.test.js
+++ __tests__/add-remove.test.js
@@ -1,6 +1,7 @@
 // import the function to be tested
 import addTask from '../src/js/modules/addTask.js';
 import removeTask from '../src/js/modules/removeTask.js';
+import clearCompleted from '../src/js/modules/clearCompleted.js';
 
 // create a mock function for localStorage
 const localStorageMock = (() => {
@@ -92,5 +93,43 @@ describe('removeTask', () => {
     expect(tasks[1].description).toBe('Task 3');
     // test that the task list contains the correct number of tasks
     expect(tasks.length).toBe(2);
+  });
+});
+
+// Test for clearCompleted function
+describe('clearCompleted', () => {
+  // clear localStorage before each test
+  beforeEach(() => {
+    window.localStorage.clear();
+  });
+  // test that the function adds a task to localStorage
+  test('Add item to localstorage', () => {
+    // add intial task list to localStorage
+    localStorage.setItem('tasks', JSON.stringify([
+      {
+        description: 'Task 1',
+        completed: true,
+        id: 1,
+      },
+      {
+        description: 'Task 2',
+        completed: true,
+        id: 2,
+      },
+      {
+        description: 'Task 3',
+        completed: false,
+        id: 3,
+      },
+    ]));
+    // retrieve task list from localStorage
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    expect(tasks.length).toBe(3);
+    // call clearCompleted function
+    clearCompleted();
+    // retrieve the updated task list from localStorage
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+    // test that the task list contains the correct tasks
+    expect(tasks.length).toBe(1);
   });
 });
