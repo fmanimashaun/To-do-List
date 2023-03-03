de89a10671bbaf6bc51971a26da3cfcf3db5cbcc
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-03 08:10:22 +0100

    Update the clearCompleted fuunction to pure function

--- src/js/modules/clearCompleted.js
+++ src/js/modules/clearCompleted.js
@@ -1,5 +1,3 @@
-import displayTodos from './displayTodos.js';
-
 const clearCompleted = () => {
   // get the task list from local storage
   const tasks = JSON.parse(localStorage.getItem('tasks'));
@@ -15,9 +13,6 @@ const clearCompleted = () => {
 
   // save the updated task list to local storage
   localStorage.setItem('tasks', JSON.stringify(uncompletedTask));
-
-  // display the updated task list
-  displayTodos();
 };
 
 export default clearCompleted;
