3d4594d36f08c0f45f6e8e900afac38846a5c22b
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-03 17:35:14 +0100

    Added the editTask function test

--- __tests__/add-remove.test.js
+++ __tests__/add-remove.test.js
@@ -1,6 +1,7 @@
 // import the function to be tested
 import addTask from '../src/js/modules/addTask.js';
 import removeTask from '../src/js/modules/removeTask.js';
+import editTask from '../src/js/modules/editTask.js';
 
 // create a mock function for localStorage
 const localStorageMock = (() => {
@@ -62,6 +63,51 @@ describe('removeTask', () => {
     window.localStorage.clear();
   });
   // test that the function adds a task to localStorage
+  test('remove item to localstorage', () => {
+    // add intial task list to localStorage
+    localStorage.setItem('tasks', JSON.stringify([
+      {
+        description: 'Task 1',
+        completed: false,
+        id: 1,
+      },
+      {
+        description: 'Task 2',
+        completed: false,
+        id: 2,
+      },
+      {
+        description: 'Task 3',
+        completed: false,
+        id: 3,
+      },
+    ]));
+
+    // retrieve task list from localStorage
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    expect(tasks.length).toBe(3);
+
+    // call removeTask function with id of task to remove
+    removeTask(2);
+
+    // retrieve task list from localStorage
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+
+    // test that the task list contains the correct tasks
+    expect(tasks[1].description).toBe('Task 3');
+
+    // test that the task list contains the correct number of tasks
+    expect(tasks.length).toBe(2);
+  });
+});
+
+// Test for editTask function
+describe('editTask', () => {
+  // clear localStorage before each test
+  beforeEach(() => {
+    window.localStorage.clear();
+  });
+  // test that the function adds a task to localStorage
   test('Add item to localstorage', () => {
     // add intial task list to localStorage
     localStorage.setItem('tasks', JSON.stringify([
@@ -81,16 +127,22 @@ describe('removeTask', () => {
         id: 3,
       },
     ]));
+
     // retrieve task list from localStorage
     let tasks = JSON.parse(localStorage.getItem('tasks'));
     expect(tasks.length).toBe(3);
-    // call removeTask function with id of task to remove
-    removeTask(2);
-    // retrieve task list from localStorage
+
+    // create the values to be passed to the editTask function
+    const id = 2;
+    const str = 'edited task';
+
+    // call editTask function with id of task to edit
+    editTask(id, str);
+
+    // retrieve the updated task list from localStorage
     tasks = JSON.parse(localStorage.getItem('tasks'));
+
     // test that the task list contains the correct tasks
-    expect(tasks[1].description).toBe('Task 3');
-    // test that the task list contains the correct number of tasks
-    expect(tasks.length).toBe(2);
+    expect(tasks[id - 1].description).toBe('edited task');
   });
 });
